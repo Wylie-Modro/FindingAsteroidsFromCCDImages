f1b907bf62ef2fd21115f20ef088319f7751d9f2
Mudit Garodia <mgarodia@ucsd.edu>
2017-03-17 22:52:14 -0700

    dataVsUSNO

--- compareDataUsno.py
+++ compareDataUsno.py
@@ -2,7 +2,11 @@ import numpy as np
 import astropy.io.fits as pf
 import matplotlib.pyplot as plt
 from bsub import bsub
-
+import urllib as url
+import math as math
+from usno import usno 
+from cmath import cos, sin, phase
+from math import radians, degrees
 
 def LocateMainPeakRanges(flatb, intensityGap, intensityThreshhold, colThreshold, rowThreshold):
         intensitiesDict = {}
@@ -169,4 +173,92 @@ peakRanges = LocateMainPeakRanges(flatb, 10., 5.0, 25, 25)
 #print("peakRanges: " + str(peakRanges))
 #print(IsolatePeaks(peakRanges))
 
+#print("peakRanges: " + str(peakRanges))
+#print(IsolatePeaks(peakRanges))
+
+fits1 = 'data-2017-03-02-nickel-Shelley.Wright/d1060.fits'
+s1 = pf.open(fits1)
+# Read position from the FITS file and convert RA/DEC to degrees
+# be sure to check that the header data is reliable. If not
+# edit the position by hand.
+ras = s1[0].header['ra']
+des = s1[0].header['dec']
+radeg = 15*(float(ras[0:2]) + float(ras[3:5])/60. + float(ras[6:])/3600.)
+dsgn = np.sign(float(des[0:2]))
+dedeg = float(des[0:2]) + dsgn*float(des[4:5])/60. + dsgn*float(des[7:])/3600.
+fovam = 3.0 # size of square search field in arc min
+epoch = s1[0].header['equinoxu']
+name,rad,ded,rmag = usno(radeg,dedeg,fovam,epoch)
+print(rad)
+
+plt.figure(1)
+w = np.where(rmag < 28)[0]
+'''
+plt.subplot(211)
+plt.plot(rad[w],ded[w],'g.')
+plt.locator_params(axis='x',nbins=4)
+plt.locator_params(axis='y',nbins=4)
+plt.tick_params('x',pad=10)
+plt.xlabel('RA [Deg]')
+plt.ylabel('Dec [Deg]')
+plt.ticklabel_format(useOffset=False)
+plt.axis('scaled')
+plt.xlim([106.0,105.0]) # reverse the x-axis direction
+'''
+
+rar0 = radians((np.amax(rad[w])+np.amin(rad[w]))/2) #RA0 in radian
+der0 = radians((np.amax(ded[w])+np.amin(ded[w]))/2) #Declination0 in radian
+
+rar = []
+der = []
+X = []
+Y = []
+x=[]
+x1=[]
+y=[]
+y1=[]
+xRotated=[]
+yRotated=[]
+for i in w:
+    rar.append(radians(rad[i])) #RA in radian
+    der.append(radians(ded[i])) #Declination in radian
+
+j = 0
+while (j < len(rar)):
+    X.append(-((cos(der[j])*sin(rar[j]-rar0))/(cos(der0)*cos(der[j])*cos(rar[j]-rar0)+sin(der[j])*sin(der0))))
+    Y.append(-((sin(der0)*cos(der[j])*cos(rar[j]-rar0)-cos(der0)*sin(der[j]))/(cos(der0)*cos(der[j])*cos(rar[j]-rar0)+sin(der[j])*sin(der0))))
+    j+=1
+
+
+k = 0
+while (k < len(X)):
+    x.append(abs(20*(X[k]/0.000015)+280))
+    y.append(abs(20*(Y[k]/0.000015)+670))        
+    k+=1
+  
+l=0
+while (l < len(x)):
+    x1.append(x[l].real)
+    y1.append(y[l].real)
+    l+=1
+
+m=0
+thetaDegrees=31
+thetaRadians=radians(thetaDegrees)
+print(type(x1))
+print(type(cos(thetaRadians)).real)
+while (m < len(x1)):
+    xRotated.append((x1[m]*(cos(thetaRadians)).real - y1[m]*(sin(thetaRadians)).real))
+    yRotated.append((x1[m]*(sin(thetaRadians)).real + y1[m]*(sin(thetaRadians)).real))
+    m+=1
+    
+print(xRotated)
+print(yRotated)
+#plt.subplot(212)
+plt.plot(xRotated, yRotated, 'b.')
+plt.xlabel('x [Pixel]')
+plt.ylabel('y [Pixel]')
+plt.xlim((0,1000))
+plt.ylim((0,1000))
+plt.show()
 
