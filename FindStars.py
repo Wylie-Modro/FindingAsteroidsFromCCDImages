5db62d190e76776f67b6dbd42a8a9464ab86148f
William Wylie-Modro <williamwyliemodro@gmail.com>
2017-03-17 18:54:28 -0700

    Finding Stars

--- FindStars.py
+++ FindStars.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from bsub import bsub
 
 
-def LocateMainPeakRanges(flatb, gap, threshhold):
+def LocateMainPeakRanges(flatb, intensityGap, intensityThreshhold, colThreshold, rowThreshold):
         intensitiesDict = {}
         rowNum = 0
         num = 0
@@ -14,8 +14,7 @@ def LocateMainPeakRanges(flatb, gap, threshhold):
             for subEntry in entry:
                 colNum += 1
                 num += 1
-                #print("rowNum: " + str(rowNum), " colNum: " + str(colNum))
-                colRowHack = int(str(rowNum)+str(colNum))
+                #print("rowNum: " + str(rowNum), " colNum: " + str(colNum)
                 #print("colRowHack : " + str(colRowHack))
                 #subEntryNpArray = np.ndarray(subEntry)
                 #print(subEntry)
@@ -23,12 +22,36 @@ def LocateMainPeakRanges(flatb, gap, threshhold):
                 #print(intensitiesDict[num])
         #print(intensitiesDict)  
         
-        draftLocationOfPeakRanges = LocateRowPeakRanges(intensitiesDict, gap, threshhold)
-        print("draftLocationOfPeakRanges: " + str(draftLocationOfPeakRanges))
-        confirmedLocationOfPeakRanges = LocateColPeakRanges(draftLocationOfPeakRanges, intensitiesDict, 5, 5)
-        print("confirmedLocationOfPeakRanges: " + str(confirmedLocationOfPeakRanges))
-
-     
+        draftLocationOfStarRanges = LocateRowPeakRanges(intensitiesDict, intensityGap, intensityThreshhold)
+        print("draftLocationOfStarRanges: " + str(draftLocationOfStarRanges))
+        print("len(draftLocationOfStarRanges): " + str(len(draftLocationOfStarRanges)))
+        #print("intensitiesDict[test]"  + str(intensitiesDict[draftLocationOfPeakRanges[0]]) + str(intensitiesDict[draftLocationOfPeakRanges[1]]) + str(intensitiesDict[draftLocationOfPeakRanges[2]]))
+        #confirmedLocationOfPeakRanges = LocateColPeakRanges(draftLocationOfPeakRanges, intensitiesDict, colThreshold, rowThreshold)
+        groupedLocationOfStarRanges = IsolateStars(draftLocationOfStarRanges, intensitiesDict, colThreshold, rowThreshold)
+        print("groupedLocationOfStasrRanges: " + str(groupedLocationOfStarRanges))
+        print("len(groupedLocationOfStarRanges): " + str(len(groupedLocationOfStarRanges)))
+        averagedStarLocations = GetAveStarLocFromAverage(groupedLocationOfStarRanges, intensitiesDict)
+        print("averagedStarLocations: " + str(averagedStarLocations))
+        print("len(averagedStarLocations): " + str(len(averagedStarLocations)))
+        
+        starRows = []
+        starCols = []
+        #starsRowsCols = np.ndarray(shape=(len(confirmedLocationOfPeakRanges), len(confirmedLocationOfPeakRanges)), dtype=int)
+        for star in averagedStarLocations:
+            #print(star)
+            starRows.append(star[0])
+            starCols.append(star[1])
+            #np.append(starsRowsCols,[(intensitiesDict[i])[0], (intensitiesDict[i])[1]])
+        print("starRows: " + str(starRows))
+        print("starCols: " + str(starCols))
+        plt.plot(starCols, starRows, "r.")
+        plt.xlim([0.0, 1024.0])
+        plt.ylim([0.0, 1024.0])
+        plt.title("intensityGap: " + str(intensityGap) + "| intensityT " + str(intensityThreshhold) + "| colT: " + str(colThreshold) + "| rowT: " + str(rowThreshold))
+        #plt.xlim([460.0, 464.0])
+        #plt.ylim([435.0, 455.0])
+        plt.show()
+        
 def LocateRowPeakRanges(intensitiesDict, gap, threshhold):
     locationRowOfPeakRanges = []
     for key, value in intensitiesDict.iteritems():
@@ -45,7 +68,8 @@ def LocateRowPeakRanges(intensitiesDict, gap, threshhold):
             endValue = (intensitiesDict[key+gap])[2]
             if midValue - startValue > threshhold and midValue - endValue > threshhold :
                 #print('Peak at:' + str(key + gap/2) +'!!!')
-                locationRowOfPeakRanges.extend([key, key + gap/2, key + gap])
+                #locationRowOfPeakRanges.extend([int(key), int(key + gap/2), int(key + gap)])
+                locationRowOfPeakRanges.append(int(key + gap/2))
             else:
                 #print('No peak at:' + str(key + gap/2)+' intensitiesDict[key+gap]: ' + str(intensitiesDict[key+gap]))
                 pass
@@ -55,58 +79,143 @@ def LocateRowPeakRanges(intensitiesDict, gap, threshhold):
 def LocateColPeakRanges(locationRowOfPeakRanges, intensitiesDict, colThreshold, rowThreshold):
     # get col and row of each
     #for keyOuterLoop, valueOuterLoop in intensitiesDict.iteritems():
-    potentialStars = []
+    potentialStarGroup = set()
     for peakNumOuterLoop in locationRowOfPeakRanges: 
         
+        #print("peakNumOuterLoop: " + str(peakNumOuterLoop))
         rowOuterLoop = (intensitiesDict[peakNumOuterLoop])[0]
-        print("rowOuterLoop: " +str(rowOuterLoop))
+        #print("rowOuterLoop: " +str(rowOuterLoop))
         colOuterLoop = (intensitiesDict[peakNumOuterLoop])[1]
         intensityOuterLoop = (intensitiesDict[peakNumOuterLoop])[2]  
           
         #for keyInnerLoop, valueInnerLoop in intensitiesDict.iteritems():
         for peakNumInnerLoop in locationRowOfPeakRanges:
             
+            #print("peakNumInnerLoop: " + str(peakNumInnerLoop))
             rowInnerLoop = (intensitiesDict[peakNumInnerLoop])[0]
-            print("rowInnerLoop: " +str(rowInnerLoop))
+            #print("rowInnerLoop: " +str(rowInnerLoop))
             colInnerLoop = (intensitiesDict[peakNumInnerLoop])[1]
             intensityInnerLoop = (intensitiesDict[peakNumInnerLoop])[2]
             
             if (rowOuterLoop != rowInnerLoop and abs(rowOuterLoop - rowInnerLoop) < rowThreshold):
                 if abs(colOuterLoop - colInnerLoop) < colThreshold:
-                    potentialStars.append([peakNumOuterLoop, peakNumInnerLoop]) 
+                    if (colOuterLoop <= 275 and colOuterLoop >= 245): #Remove Line
+                        pass
+                    else: 
+                        #potentialStars.add([peakNumOuterLoop, peakNumInnerLoop]) 
+                        potentialStarGroup.add(peakNumOuterLoop)
+                        potentialStarGroup.add(peakNumInnerLoop)
+                else:
+                    #add to new set
+                    pass
     
-    return potentialStars
+    return potentialStarGroup
     
     # compare col with another, if close and if row is close and add range to another list  
     
     
 
-def IsolatePeaks(peakRanges):
+def IsolateStars(locationRowOfPeakRanges, intensitiesDict, colThreshold, rowThreshold):
         peakSet = set()
         masterList = []
         tempList = []
-        for peak1 in peakRanges:
-            if np.abs(peak1 - peakRanges[peakRanges.index(peak1)+1]) < 10:
-                peakSet.add(peak1)
-                peakSet.add(peakRanges[peakRanges.index(peak1)+1])
+        
+        rows = []
+        cols=[]
+        #print("locationRowOfPeakRanges before: " + str(locationRowOfPeakRanges))
+        locationRowOfPeakRanges = sorted(locationRowOfPeakRanges)
+        #print("locationRowOfPeakRanges after: " + str(locationRowOfPeakRanges))
+        
+        
+        for peakNumOuterLoop in locationRowOfPeakRanges:
+            rowO = (intensitiesDict[peakNumOuterLoop])[0]
+            colO = (intensitiesDict[peakNumOuterLoop])[1]
+            #print("----------------------------------")
+            for peakNumInnerLoop in locationRowOfPeakRanges:
+           
+           
+            print("peakNumOuterLoop: " + str(peakNumOuterLoop))
+            #nextRow = (intensitiesDict[locationRowOfPeakRanges[locationRowOfPeakRanges.index(peakNumOuterLoop)+1]])[0]
+            print("row: " + str(row))
+            #print("nextRow: " + str(nextRow))
+            
+            #nextCol = (intensitiesDict[locationRowOfPeakRanges[locationRowOfPeakRanges.index(peakNumOuterLoop)+1]])[1]
+            
+            print("col: " + str(col))
+            #print("nextCol: " + str(nextCol))
+            
+            if np.abs(row - nextRow) <= rowThreshold and np.abs(col - nextCol) <= colThreshold:
+                if (col <= 275 and col >= 245): #Remove Line
+                    pass
+                else:
+                    peakSet.add(peakNumOuterLoop)
+                    #print("Added peakNumOuterLoop: " + str(peakNumOuterLoop))
+                    peakSet.add(locationRowOfPeakRanges[locationRowOfPeakRanges.index(peakNumOuterLoop)+1])
+                    #print("Added locationRowOfPeakRanges[locationRowOfPeakRanges.index(peakNumOuterLoop)+1]: " + str(locationRowOfPeakRanges[locationRowOfPeakRanges.index(peakNumOuterLoop)+1]))
+                    #print("peakSet after add: " + str(peakSet))
             else: 
                 if len(peakSet) > 0:
+                    # check if something close is in masterList
+
                     for peak in peakSet:
+                        #print("peakSet: " + str(peakSet))
                         tempList.append(peak)
                     copyOftempList = list(tempList)
+                    #print("tempList: "  + str(tempList))
                     masterList.append(copyOftempList)
+                    #print("masterList: " + str(masterList))
                     del tempList[:]
                     peakSet.clear()
             
-            if peakRanges[peakRanges.index(peak1)+1] == peakRanges[-1]:
+            lastRow = (intensitiesDict[locationRowOfPeakRanges[-1]])[0]
+            lastCol = (intensitiesDict[locationRowOfPeakRanges[-1]])[1]
+            #print("lastRow: " + str(lastRow))
+            #print("lastCol: " + str(lastCol))
+            if nextRow == lastRow and nextCol == lastCol:
                 for peak in peakSet:
                     tempList.append(peak)
                 masterList.append(tempList)
-                return masterList      
+                
+                print("About to exit")
+                return masterList    
+           # print("----------------------------------")
         
 
+def MergeGroupsThatAreClose(intensitiesDict, masterList, colThreshold, rowThreshold):
+    finalGroups = []
+    for group1 in masterList:
+        for pixel1 in group1:
+            
+            row1 = (intensitiesDict[pixel1])[0]
+            col1 = (intensitiesDict[pixel1])[1]
+            
+            for group2 in masterList:
+                for pixel2 in group2:
+                    
+                    row2 = (intensitiesDict[pixel2])[0]
+                    col2 = (intensitiesDict[pixel2])[1]
+                    
+                    if (abs(row1 - row2) < rowThreshold and abs(col1 - col2) < colThreshold) and group1 != group2:
+                        pass
+                        
+                
 
 
+def GetAveStarLocFromAverage(masterList, intensitiesDict):
+    listOfAveragedStarPositions = []
+    for group in masterList:
+        #print("group: " + str(group))
+        sumCol = 0
+        sumRow = 0
+        for value in group:
+            #print("value: " + str(value))
+            sumRow += (intensitiesDict[value])[0]
+            sumCol += (intensitiesDict[value])[1]
+        if len(group) > 0:
+            groupRowAverage = sumRow / len(group)
+            groupColAverage = sumCol / len(group)
+            listOfAveragedStarPositions.append([groupRowAverage, groupColAverage])
+    return listOfAveragedStarPositions
 
 print('Asteroids do not concern me, Admiral. - Darth Vader')
 
@@ -123,16 +232,8 @@ flatb = bsub(flat,hdr.get('cover')) # Bias subtract
 flatb = flatb/np.median(flatb) # normalize
 
 
-print(flatb.size)
-
-i = 0
-for entry in flatb:
-    i+=1
-print(i)
-print(i**2)
-
-peakRanges = LocateMainPeakRanges(flatb, 10., 0.5)
-print("peakRanges: " + str(peakRanges))
+peakRanges = LocateMainPeakRanges(flatb, 10., 10.0, 200, 200) # Recommend threshholf of about 600
+#print("peakRanges: " + str(peakRanges))
 #print(IsolatePeaks(peakRanges))
 
 
